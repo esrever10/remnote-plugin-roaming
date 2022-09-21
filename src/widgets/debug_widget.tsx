01eb0967448281ce1519e7985e507e718f7a40e6
stringnet <caochen@cloudist.cc>
2022-09-21 12:25:34 +0800

    first blood

---
+++ src/widgets/debug_widget.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  AppEvents,
+  LoadingSpinner,
+  RemHierarchyEditorTree,
+  RemRichTextEditor,
+  RemViewer,
+  renderWidget,
+  RichTextElementTextInterface,
+  RichTextInterface,
+  useAPIEventListener,
+  useLocalStorageState,
+  usePlugin,
+  useRunAsync,
+  useSyncedStorageState,
+  WidgetLocation,
+} from "@remnote/plugin-sdk";
+import * as Re from "remeda"
+
+const { useState, useEffect } = React;
+
+function richText2log(text: RichTextInterface) {
+    const values = Array.from(text.values());
+    return values.map((item) => {
+      switch(item.i) {
+        case undefined:
+          return item;
+        case 's':
+          return `(s, ${item.delimiterCharacterForSerialization})`
+        case 'm':
+        case 'n':
+        case 'x':
+          return `(${item.i}, ${item.text})`
+        default:
+          return `(${item.i},)`
+      }
+    }).join(' | ');
+  }
+
+function DebugWidget() {
+  const [remId, setRemId] = useState<string>("");
+  const [info, setInfo] = useState<Map<string, any>>(new Map());
+  const plugin = usePlugin();
+
+  useAPIEventListener(AppEvents.FocusedRemChange, undefined, async (args) => {
+    console.log(args);
+    const remId = args.nextRemId;
+    setRemId(remId);
+
+    const rem = await plugin.rem.findOne(remId);
+    setInfo(new Map());
+    if (rem) {
+        info?.set("isPowerupEnum", await rem?.isPowerupEnum())
+        info?.set("isPowerupProperty", await rem?.isPowerupProperty())
+        info?.set("isPowerup", await rem?.isPowerup())
+        info?.set("isPowerupPropertyListItem", await rem?.isPowerupPropertyListItem())
+        info?.set("isPowerupSlot", await rem?.isPowerupSlot())
+        info?.set("isSlot", await rem?.isSlot())
+        info?.set("isDocument", await rem?.isDocument())
+        info?.set("parent-isPowerupSlot",await (await rem.getParentRem())?.isPowerupSlot())
+        info?.set("hasPowerup:f", await rem?.hasPowerup('f'))
+        info?.set("hasPowerup:z", await rem?.hasPowerup('z'))
+        info?.set("parent-hasPowerup:f", await (await rem?.getParentRem())?.hasPowerup('f'))
+        info?.set("parent-hasPowerup:z", await (await rem?.getParentRem())?.hasPowerup('z'))
+        info?.set("parent-parent-hasPowerup:z", await (await (await rem.getParentRem())?.getParentRem())?.hasPowerup('f'))
+        
+        info?.set("richtext", richText2log(rem.text));
+        setInfo(info);
+    }
+  })
+
+  return (
+    <div>
+        <RemViewer remId={remId} width='50%'></RemViewer>
+        {info ? 
+            <ul>
+                {Re.map.indexed(Array.from(info?.keys()!), (x, i) => 
+                    <li key={i}>
+                        {x}: {JSON.stringify(info?.get(x))}
+                    </li>
+                )}
+            </ul>
+        :""}
+        
+    </div>
+  );
+}
+
+renderWidget(DebugWidget);
--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,45 +1,46 @@
-import { declareIndexPlugin, ReactRNPlugin, WidgetLocation } from '@remnote/plugin-sdk';
+import { AppEvents, declareIndexPlugin, ReactRNPlugin, WidgetLocation } from '@remnote/plugin-sdk';
 import '../style.css';
 import '../App.css';
 
 async function onActivate(plugin: ReactRNPlugin) {
-  // Register settings
+  await plugin.app.registerWidget(
+    "roaming_widget",
+    WidgetLocation.SidebarEnd,
+    {
+      dimensions: { height: "auto", width: "280px" },
+    }
+  );
+  await plugin.app.registerWidget(
+    "debug_widget",
+    WidgetLocation.RightSidebar,
+    {
+      dimensions: { height: "auto", width: "100%" },
+    }
+  );  
+
   await plugin.settings.registerStringSetting({
-    id: 'name',
-    title: 'What is your Name?',
-    defaultValue: 'Bob',
-  });
-
-  await plugin.settings.registerBooleanSetting({
-    id: 'pizza',
-    title: 'Do you like pizza?',
-    defaultValue: true,
-  });
-
-  await plugin.settings.registerNumberSetting({
-    id: 'favorite-number',
-    title: 'What is your favorite number?',
-    defaultValue: 42,
-  });
-
-  // A command that inserts text into the editor if focused.
-  await plugin.app.registerCommand({
-    id: 'editor-command',
-    name: 'Editor Command',
-    action: async () => {
-      plugin.editor.insertPlainText('Hello World!');
-    },
-  });
-
-  // Show a toast notification to the user.
-  await plugin.app.toast("I'm a toast!");
-
-  // Register a sidebar widget.
-  await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
-    dimensions: { height: 'auto', width: '100%' },
+    id: "level_custom",
+    title: "Custom level.",
+    defaultValue: 
+`0::NoName
+100::Underdog
+500::Rookie
+1000::SmartKid
+2000::Professional
+4000::BountyHunter
+8000::SmartAss
+10000::Hurricane
+12000::SuperStar
+14000::Jack
+16000::King
+20000::Ace
+50000::NoName`,
+    multiline: true,
   });
 }
 
 async function onDeactivate(_: ReactRNPlugin) {}
 
 declareIndexPlugin(onActivate, onDeactivate);
+
+
